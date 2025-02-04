6f6a80f34b31a52902eaa3b2659ec825d6ba8c8e
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-02-04 10:50:33 +0900

    delete 수정

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { S3 } from 'aws-sdk';
 import { ConfigService } from '@nestjs/config';
-import { Post, PostDocument } from './schemas/post.schema';
+import { Post, PostDocument } from '../schemas/post.schema';
 import { CreatePostDto } from './dto/post.dto';
 import {
   NotFoundException,
@@ -59,10 +59,19 @@ export class PostService {
   }
 
   async delete(id: string): Promise<{ success: boolean; message: string }> {
-    const deleted = await this.postModel.findByIdAndDelete(id);
-    if (!deleted) {
+    const post = await this.postModel.findByIdAndDelete(id);
+    if (!post) {
       throw new NotFoundException('Post not found');
     }
+
+    if (post.image) {
+      const key = post.image.split('.com/')[1];
+      await this.s3
+        .deleteObject({ Bucket: this.bucketName, Key: key })
+        .promise();
+    }
+
+    await this.postModel.findByIdAndDelete(id);
     return { success: true, message: 'Post deleted successfully' };
   }
 }
