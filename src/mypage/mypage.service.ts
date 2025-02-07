f57b9512cc816ed9c5e4767627a25411999e9ad1
hayoung <jbg01200249@gmail.com>
2025-02-07 22:05:06 +0900

    주석 수정

--- src/mypage/mypage.service.ts
+++ src/mypage/mypage.service.ts
@@ -4,8 +4,8 @@ import { CreateMypageDto } from './dto/mypage.dto';
 @Injectable()
 export class MypageService {
   async create(createMypageDto: CreateMypageDto) {
-    // DB 저장 등을 진행
-    // 예시로 데이터를 로그에 출력하고, 성공 메시지를 반환
+    // DB 저장 진행
+    // 예시로 데이터를 로그 출력, 성공 메시지를 반환
     console.log('Received data:', createMypageDto);
     return {
       message: '데이터가 성공적으로 처리되었습니다.',
